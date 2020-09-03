e1e2dd58f69910fb4c0a7dcd572df7138b9af664
Kei Mochizuki <keimochizuki@med.kindai.ac.jp>
2020-09-03 16:56:06 +0900

    Updated man

--- extras/man/search/all_c.js
+++ extras/man/search/all_c.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['ulong_5fmax',['ULONG_MAX',['../cgnuino_8h.html#a6c5728776beef0f7bc53db8f18944517',1,'cgnuino.h']]],
   ['until',['until',['../classCgnPeriod.html#a3aa632ae76af8ae55d0733a75471c107',1,'CgnPeriod']]],
-  ['update',['update',['../classCgnControl.html#af6b75faef7e2c52a40446aad5f41bc26',1,'CgnControl::update()'],['../classCgnDI.html#a95e8d70a404b9d6b05c562cc293ec39c',1,'CgnDI::update()'],['../classCgnDO.html#a82059b0f49870691dbd708d546f495c6',1,'CgnDO::update()'],['../classCgnLogger.html#a10b9f485cf92d3c0922cdcbfabaebe73',1,'CgnLogger::update()'],['../classCgnTone.html#a70c812f5ee77a54a9f7d5be3fd6c9547',1,'CgnTone::update()']]]
+  ['update',['update',['../classCgnControl.html#a4b2bbe20e02aac418f974a8588193fcd',1,'CgnControl::update()'],['../classCgnDI.html#a95e8d70a404b9d6b05c562cc293ec39c',1,'CgnDI::update()'],['../classCgnDO.html#a82059b0f49870691dbd708d546f495c6',1,'CgnDO::update()'],['../classCgnLogger.html#a10b9f485cf92d3c0922cdcbfabaebe73',1,'CgnLogger::update()'],['../classCgnTone.html#a70c812f5ee77a54a9f7d5be3fd6c9547',1,'CgnTone::update()']]]
 ];
